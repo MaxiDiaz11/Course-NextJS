011219903c569cc5ca63d871ff49f3a3279f1f89
Maxi Diaz <maxi.diaz66@gmail.com>
2023-01-12 19:34:03 -0300

    feat: pokemon-static project

---
+++ pokemon-static/pages/index.tsx
@@ -0,0 +1,12 @@
+import { NextPage } from "next";
+import { Button } from "@nextui-org/react";
+
+const HomePage: NextPage = () => {
+  return (
+    <>
+      <Button color="gradient">Hola mundo</Button>
+    </>
+  );
+};
+
+export default HomePage;
